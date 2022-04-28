f457f696d1b6aabfaed05e0258f0351031772ac7
Meawooo <96882676+Meawooo@users.noreply.github.com>
2022-04-28 21:26:19 +0530

    Update ads.js

--- src/bot/src/handlers/commands/ads.js
+++ src/bot/src/handlers/commands/ads.js
@@ -17,7 +17,7 @@ module.exports = {
         .setTitle('#Rule 5')
         .setColor('#2f3136')
         .setFooter(`Requested By ${message.author.tag}`, message.author.displayAvatarURL())
-        .setDescription(`Botrix allows you to advertise in our server for free in ${chan}, but you can always list your server on our site [here](https://botrix.cc/servers).\n\n**Server Rule :**\`\`\`fix\nIf you want to advertise, send your ad in #│self-promo. [ Remember if it's a botlist promo or any kind of NSFW promo. Our staff team has the right to remove it. ]\`\`\``)
+        .setDescription(`Tumull allows you to advertise in our server for free in ${chan}, but you can always list your server on our site [here](https://tumull.casino/servers).\n\n**Server Rule :**\`\`\`fix\nIf you want to advertise, send your ad in #│self-promo. [ Remember if it's a botlist promo or any kind of NSFW promo. Our staff team has the right to remove it. ]\`\`\``)
         message.channel.send(rule3)
     }catch(err){
         message.channel.send(`Oops! looks like an error occurred : \`${err.message}\``)
