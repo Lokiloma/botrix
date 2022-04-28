bf883cdc75564d156b8ec306c6ad7ed77e5a3fe4
Meawooo <96882676+Meawooo@users.noreply.github.com>
2022-04-28 21:00:26 +0530

    Update ready.js

--- src/bslBot/events/ready.js
+++ src/bslBot/events/ready.js
@@ -5,5 +5,5 @@ const eventFiles2 = readdirSync(filePath2);
 const BotrixLogs = console.log;
 
 module.exports = (client) => {
-    client.user.setPresence({ activity: { name: 'https://botrix.cc/servers', type: "WATCHING" }, status: 'idle' })
+    client.user.setPresence({ activity: { name: 'https://tumull.casino', type: "PLAYING" }, status: 'idle' })
 }
